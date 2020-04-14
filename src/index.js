c2e3fc4c7071393815ac6be8c4e74ea886a6a968
Cody <codysolomon15@gmail.com>
2020-04-14 14:05:12 -0700

    completed project, no stretch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import myReducer from './reducers/Reducer';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(myReducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+
+, rootElement);
